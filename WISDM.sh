6198658afb9e09ded5c2416e1459ee027f9a2c47
Mahyad Saedpanah <saedpanahmahyad@gmail.com>
2025-11-14 03:47:35 +0330

    update WISDM.sh

--- WISDM.sh
+++ WISDM.sh
@@ -8,7 +8,8 @@ CUDA_VISIBLE_DEVICES=0 python main.py \
  --lr 0.003 \
  --cls_trade_off 1 \
  --domain_trade_off 1 \
- --entropy_trade_off 0.01 \
+ --entropy_trade_off 0.02 \
  --align_t_trade_off 1 \
  --align_s_trade_off 1 \
+ --kl_t 2.0 \
  --device cuda \
